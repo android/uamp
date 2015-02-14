39a49be4c497c8278d1aec476be6080a3a3e9732
Renato Mangini <mangini@google.com>
2015-02-13 18:52:12 -0800

    Fix margins, styles, colors, etc.
    Bug: 19300873
    Bug: 19300743
    
     - Second row of text in each list item is too far down, should have no extra
     padding between first and second rows (should just be two adjacent
     height=wrap_content TextViews)
     - For songs, icons should be offset from left at 16dp, text at 72dp
     - Set app:contentInsetStart=72dp for all Toolbars
     - Change the equalizer animation to an accent color like teal or dark cyan.
     - Music title aligned to toolbar
     - tablet: add more margin like inbox does.
    
    Change-Id: Id28cea83a0380a4918aefc0a942fea98bb38e02d

--- mobile/src/main/java/com/example/android/uamp/ui/MediaItemViewHolder.java
+++ mobile/src/main/java/com/example/android/uamp/ui/MediaItemViewHolder.java
@@ -16,6 +16,8 @@
 package com.example.android.uamp.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaDescription;
 import android.view.LayoutInflater;
@@ -34,12 +36,20 @@ public class MediaItemViewHolder {
     static final int STATE_PAUSED = 2;
     static final int STATE_PLAYING = 3;
 
+    private static ColorStateList sColorStatePlaying;
+    private static ColorStateList sColorStateNotPlaying;
+
     ImageView mImageView;
     TextView mTitleView;
     TextView mDescriptionView;
 
     static View setupView(Activity activity, View convertView, ViewGroup parent,
                                     MediaDescription description, int state) {
+
+        if (sColorStateNotPlaying == null || sColorStatePlaying == null) {
+            initializeColorStateLists(activity);
+        }
+
         MediaItemViewHolder holder;
 
         Integer cachedState = STATE_INVALID;
@@ -66,19 +76,22 @@ public class MediaItemViewHolder {
             switch (state) {
                 case STATE_PLAYABLE:
                     holder.mImageView.setImageDrawable(
-                            activity.getDrawable(R.drawable.ic_play_arrow_white_24dp));
+                        activity.getDrawable(R.drawable.ic_play_arrow_black_36dp));
+                    holder.mImageView.setImageTintList(sColorStateNotPlaying);
                     holder.mImageView.setVisibility(View.VISIBLE);
                     break;
                 case STATE_PLAYING:
                     AnimationDrawable animation = (AnimationDrawable)
-                            activity.getDrawable(R.drawable.ic_equalizer_white_24dp);
+                        activity.getDrawable(R.drawable.ic_equalizer_white_36dp);
                     holder.mImageView.setImageDrawable(animation);
+                    holder.mImageView.setImageTintList(sColorStatePlaying);
                     holder.mImageView.setVisibility(View.VISIBLE);
                     animation.start();
                     break;
                 case STATE_PAUSED:
                     holder.mImageView.setImageDrawable(
-                            activity.getDrawable(R.drawable.ic_equalizer1_white_24dp));
+                        activity.getDrawable(R.drawable.ic_equalizer1_white_36dp));
+                    holder.mImageView.setImageTintList(sColorStateNotPlaying);
                     holder.mImageView.setVisibility(View.VISIBLE);
                     break;
                 default:
@@ -90,4 +103,10 @@ public class MediaItemViewHolder {
         return convertView;
     }
 
+    static private void initializeColorStateLists(Context ctx) {
+        sColorStateNotPlaying = ColorStateList.valueOf(ctx.getResources().getColor(
+            R.color.media_item_icon_not_playing));
+        sColorStatePlaying = ColorStateList.valueOf(ctx.getResources().getColor(
+            R.color.media_item_icon_playing));
+    }
 }
