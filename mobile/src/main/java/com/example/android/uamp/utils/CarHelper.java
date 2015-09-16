331205c1dc116d148703caaa9bb681be4ac2820e
santosh-kumar-kaushal <santoshkumarmuz77@gmail.com>
2015-09-15 19:08:41 -0700

    Add broadcastreceiver to detect car connection.
    Use a BroadcastReceiver to set a flag when an Android Auto compatible car is
    connected to the media app.
    
    Bug: 23597698
    Change-Id: Ie8fcb25e758c56b1aeb8b0fa0ff7a1c46aaf60fb

--- mobile/src/main/java/com/example/android/uamp/utils/CarHelper.java
+++ mobile/src/main/java/com/example/android/uamp/utils/CarHelper.java
@@ -20,6 +20,8 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
+import com.example.android.uamp.MusicService;
+
 public class CarHelper {
     private static final String TAG = LogHelper.makeLogTag(CarHelper.class);
 
@@ -33,6 +35,38 @@ public class CarHelper {
             "com.google.android.gms.car.media.ALWAYS_RESERVE_SPACE_FOR.ACTION_SKIP_TO_PREVIOUS";
     private static final String SLOT_RESERVATION_QUEUE =
             "com.google.android.gms.car.media.ALWAYS_RESERVE_SPACE_FOR.ACTION_QUEUE";
+
+    /**
+     * Action for an intent broadcast by Android Auto when a media app is connected or
+     * disconnected. A "connected" media app is the one currently attached to the "media" facet
+     * on Android Auto. So, this intent is sent by AA on:
+     *
+     * - connection: when the phone is projecting and at the moment the app is selected from the
+     *       list of media apps
+     * - disconnection: when another media app is selected from the list of media apps or when
+     *       the phone stops projecting (when the user unplugs it, for example)
+     *
+     * The actual event (connected or disconnected) will come as an Intent extra,
+     * with the key MEDIA_CONNECTION_STATUS (see below).
+     */
+    public static final String ACTION_MEDIA_STATUS = "com.google.android.gms.car.media.STATUS";
+
+    /**
+     * Key in Intent extras that contains the media connection event type (connected or disconnected)
+     */
+    public static final String MEDIA_CONNECTION_STATUS = "media_connection_status";
+
+    /**
+     * Value of the key MEDIA_CONNECTION_STATUS in Intent extras used when the current media app
+     * is connected.
+     */
+    public static final String MEDIA_CONNECTED = "media_connected";
+
+    /**
+     * Value of the key MEDIA_CONNECTION_STATUS in Intent extras used when the current media app
+     * is disconnected.
+     */
+    public static final String MEDIA_DISCONNECTED = "media_disconnected";
 
 
     public static boolean isValidCarPackage(String packageName) {
@@ -60,6 +94,11 @@ public class CarHelper {
 
     /**
      * Returns true when running Android Auto or a car dock.
+     *
+     * A preferable way of detecting if your app is running in the context of an Android Auto
+     * compatible car is by registering a BroadcastReceiver for the action
+     * {@link CarHelper#ACTION_MEDIA_STATUS}. See a sample implementation in
+     * {@link MusicService#onCreate()}.
      *
      * @param c Context to detect UI Mode.
      * @return true when device is running in car mode, false otherwise.
