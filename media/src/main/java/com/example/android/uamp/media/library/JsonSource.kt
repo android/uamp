3f8ff3b605f9343c4bf487e8a51d3173e7ada080
Nicole Borrelli <borrelli@google.com>
2018-05-18 16:06:52 -0700

    Add support for browsable items to MusicService.
    - Create a "BrowseTree" which holds the hierarchy of items from a root,
    to list of albums in the music source, to songs on the album (initially).
    - Adds more documentation for onGetRoot and onLoadChildren in MusicService.
    - Minor updates to support browsing.
    
    Change-Id: Id931ad615ea1d312418b608927e7c4d8702dbe31

--- media/src/main/java/com/example/android/uamp/media/library/JsonSource.kt
+++ media/src/main/java/com/example/android/uamp/media/library/JsonSource.kt
@@ -19,6 +19,7 @@ package com.example.android.uamp.media.library
 import android.content.Context
 import android.net.Uri
 import android.os.AsyncTask
+import android.support.v4.media.MediaBrowserCompat.MediaItem
 import android.support.v4.media.MediaMetadataCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
@@ -34,6 +35,7 @@ import com.example.android.uamp.media.extensions.displayIconUri
 import com.example.android.uamp.media.extensions.displaySubtitle
 import com.example.android.uamp.media.extensions.displayTitle
 import com.example.android.uamp.media.extensions.duration
+import com.example.android.uamp.media.extensions.flag
 import com.example.android.uamp.media.extensions.genre
 import com.example.android.uamp.media.extensions.id
 import com.example.android.uamp.media.extensions.mediaUri
@@ -141,6 +143,7 @@ fun MediaMetadataCompat.Builder.from(jsonMusic: JsonMusic): MediaMetadataCompat.
     albumArtUri = jsonMusic.image
     trackNumber = jsonMusic.trackNumber
     trackCount = jsonMusic.totalTrackCount
+    flag = MediaItem.FLAG_PLAYABLE
 
     // To make things easier for *displaying* these, set the display properties as well.
     displayTitle = jsonMusic.title
