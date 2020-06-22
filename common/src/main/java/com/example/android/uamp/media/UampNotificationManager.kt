92e4a07b0487d9938938aedcdb57d4e52a6aeaac
Don Turner <donturner@google.com>
2020-06-22 11:59:01 +0100

    WIP: Add cast support

--- common/src/main/java/com/example/android/uamp/media/UampNotificationManager.kt
+++ common/src/main/java/com/example/android/uamp/media/UampNotificationManager.kt
@@ -42,7 +42,7 @@ const val NOW_PLAYING_NOTIFICATION_ID = 0xb339 // Arbitrary number used to ident
  */
 class UampNotificationManager(
     private val context: Context,
-    private val player: ExoPlayer,
+    private val player: Player,
     sessionToken: MediaSessionCompat.Token,
     notificationListener: PlayerNotificationManager.NotificationListener
 ) {
