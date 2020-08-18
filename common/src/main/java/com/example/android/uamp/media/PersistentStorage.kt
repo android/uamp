e0398ae28e61e7edd4509e51bc8fe2ef94ae88e0
santosh-kumar-kaushal <santoshkumarmuz77@gmail.com>
2020-08-18 12:33:44 +0100

    [Playback resumption] Store recent song in shared prefs

---
+++ common/src/main/java/com/example/android/uamp/media/PersistentStorage.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.uamp.media
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
+import android.support.v4.media.MediaDescriptionCompat
+
+class PersistentStorage private constructor(val context: Context) {
+
+    /**
+     * Store any data which must persist between restarts, such as the most recently played song
+     * , in shared preferences.
+     */
+    private var preferences: SharedPreferences = context.getSharedPreferences(
+        PREFERENCES_NAME,
+        Context.MODE_PRIVATE
+    )
+
+    companion object {
+
+        @Volatile
+        private var instance: PersistentStorage? = null
+
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: PersistentStorage(context).also { instance = it }
+            }
+    }
+
+    fun saveRecentSong(description: MediaDescriptionCompat) {
+        preferences.edit()
+            .putString(RECENT_SONG_MEDIA_ID_KEY, description.mediaId)
+            .putString(RECENT_SONG_TITLE_KEY, description.title.toString())
+            .putString(RECENT_SONG_SUBTITLE_KEY, description.subtitle.toString())
+            .putString(RECENT_SONG_ICON_URI_KEY, description.iconUri.toString())
+            .apply()
+    }
+
+    fun loadRecentSong(): MediaBrowserCompat.MediaItem? {
+        val mediaId = preferences.getString(RECENT_SONG_MEDIA_ID_KEY, null)
+        return if (mediaId == null) {
+            null
+        } else {
+            MediaBrowserCompat.MediaItem(
+                MediaDescriptionCompat.Builder()
+                    .setMediaId(mediaId)
+                    .setTitle(preferences.getString(RECENT_SONG_TITLE_KEY, ""))
+                    .setSubtitle(preferences.getString(RECENT_SONG_SUBTITLE_KEY, ""))
+                    .setIconUri(Uri.parse(preferences.getString(RECENT_SONG_ICON_URI_KEY, "")))
+                    .build(), FLAG_PLAYABLE
+            )
+        }
+    }
+}
+
+private const val PREFERENCES_NAME = "uamp"
+private const val RECENT_SONG_MEDIA_ID_KEY = "recent_song_media_id"
+private const val RECENT_SONG_TITLE_KEY = "recent_song_title"
+private const val RECENT_SONG_SUBTITLE_KEY = "recent_song_subtitle"
+private const val RECENT_SONG_ICON_URI_KEY = "recent_song_icon_uri"
