19820327a8265368de6ab54c3a44700be7b1e19f
Nicole Borrelli <borrelli@google.com>
2020-12-22 10:49:45 +0000

    WIP to use content:// Uris for album art.
    This addresses #235
    
    At the moment, the first time art is retrieved it doesn't appear.
    Subsequent loads will show the images.
    
    This is likely related to Glide not observing the Uri for changes. This
    might work on other form factors (for example, Automotive OS) but it
    also might be necessary to update the MediaItems themselves.
    
    The change is probably going to wait until January 2021 to be resolved.

--- common/src/main/java/com/example/android/uamp/media/extensions/FileExt.kt
+++ common/src/main/java/com/example/android/uamp/media/extensions/FileExt.kt
@@ -35,4 +35,4 @@ fun File.asAlbumArtContentUri(): Uri {
         .build()
 }
 
-private const val AUTHORITY = "com.example.android.uamp.media.library.provider"
+private const val AUTHORITY = "com.example.android.uamp"
